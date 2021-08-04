a624f0ce656fb3fdd8cddcd8b4da3b97c79afe75
Ben Bauer <ben.bauer1@gmail.com>
2021-08-04 12:46:25 -0500

    updated pictures

--- main.py
+++ main.py
@@ -11,13 +11,18 @@ correct = '<h1>You are Correct!</h1><p>Please refresh to play again!</p> <img sr
 
 low = '<h1>Too low!</h1><p>Please guess again!</p> <img src="https://media.giphy.com/media/3OhXBaoR1tVPW/giphy.gif">'
 
-high = '<h1>Too high!</h1><p>Please guess again!</p> <img src="https://media.giphy.com/media/9DlCEg3G79DY0jFlMo/giphy.gif">'
+high = '<h1>Too high!</h1><p>Please guess again!</p> <img src="https://media.giphy.com/media/zu8DrkFiuz8JO/giphy.gif">'
 
-@app.route(/<number>)
+@app.route('/')
+def home():
+    return "<p>Please add a number after the URL between 1 and 10</p>"
+
+
+@app.route('/<number>')
 def isCorrect(number):
-    if(number == randnumber):
+    if(int(number) == randnumber):
         return correct
-    elif(number > randnumber):
+    elif(int(number) > randnumber):
         return high
     else:
         return low
